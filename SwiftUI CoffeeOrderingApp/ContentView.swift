47b8fd3acdba7b3f205cc892afa34ab8fe83d000
Dmitry Novosyolov <i1710@icloud.com>
2019-08-25 12:43:23 +0300

    Initial Commit

---
+++ SwiftUI CoffeeOrderingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI CoffeeOrderingApp
+//
+//  Created by Dmitry Novosyolov on 25/08/2019.
+//  Copyright © 2019 Dmitry Novosyolov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
